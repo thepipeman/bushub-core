8648868cc407939dd676634f48fdb1f2c9155050
Aaron Rasing <aaron.rasing@nextbank.ph>
2023-07-14 21:00:24 +0800

    Scaffold

---
+++ src/main/java/com/bushub/core/BushubCoreApplication.java
@@ -0,0 +1,13 @@
+package com.bushub.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BushubCoreApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(BushubCoreApplication.class, args);
+  }
+
+}
